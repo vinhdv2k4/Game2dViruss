2480f204c7cf07499cec07364756d0adb9c0366e
FamiHust <duc.vm224305@gmail.com>
2025-05-18 11:48:59 +0700

    update

---
+++ Assets/Scripts/PlayerStateList.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HUST
+{
+    public class PlayerStateList : MonoBehaviour
+    {
+        public bool isJumping = false;
+        public bool isDashing = false;
+    }
+}
+
